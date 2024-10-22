d79363d1440508b89c1bc223e8dafe514e681bc0
giuseppe <giuseppefernando09@gmail.com>
2024-10-22 19:40:49 -0300

    Pagina Home, onde vai ficar a hub de projetos

---
+++ ABA/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import Nav from 'react-bootstrap/Nav';
+
+function Header() {
+  return (
+    <>
+    <div className='navbar'>
+    <Nav
+      activeKey="/home"
+      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
+    >
+      <Nav.Item>
+        <Nav.Link href="/home">Active</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-2">Link</Nav.Link>
+      </Nav.Item>
+     </Nav>
+    </div>
+    </>
+  );
+}
+
+export default Header;
