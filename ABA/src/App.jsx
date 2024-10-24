d9fbc7cbaa36618d807ed32ebbcbbe15b45ffab4
Giuseppe <giuseppefernando09@gmail.com>
2024-10-24 16:36:35 -0300

    Adicionado o nome da pagina e a logo do projeto, ajustes no card da pagina home e tela para ver as instituições praticamente feita.

--- ABA/src/App.jsx
+++ ABA/src/App.jsx
@@ -3,13 +3,14 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import Instituicao from './components/Instituiçoes';
 
 function App() {
 
   return (
     <>
        <Header/>
-      <Home/>
+      <Instituicao/>
     </>
   )
 }
