e0d4885e5ffec2458625b55a8156551cf96da904
Giuseppe Fernando <giuseppefernando09@gmail.com>
2024-11-25 17:04:33 -0300

    modificaçoes

--- ABA/src/App.tsx
+++ ABA/src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
   
   return (
     <>
-    <Instituicoes/>
+    <PerfilAdm/>
     </>
   );
 };
--- ABA/src/pages/PerfilAdm.tsx
+++ ABA/src/pages/PerfilAdm.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BarraLateral from "../components/BarraLateral";
 import Sidebar from "../components/SidebarCadastro";
+import Graficos from "../components/Graficos";
 
 const PerfilAdm  =()=> {
     return(
         <>
+        <Graficos/>
         <BarraLateral/>
         <Sidebar/>
         </>
